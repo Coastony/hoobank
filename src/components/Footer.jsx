8c595276b6e8365e55e37b67cfa53a08f396a100
Coastony <sergiobsantos8@gmail.com>
2023-03-10 12:31:07 -0300

    Primeiro Commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => (
         <img
           src={logo}
           alt="hoobank"
-          className="w-[266px] h-[72.14px] object-contain"
+          className="w-[266px] h-[72.14px] object-contain cursor-pointer"
         />
         <p className={`${styles.paragraph} mt-4 max-w-[312px]`}>
           A new way to make the payments easy, reliable and secure.
